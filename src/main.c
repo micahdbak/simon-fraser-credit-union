321eb2788cc89b4eeb8ad543882c616eae73f4eb
Micah Baker <micahb923@gmail.com>
2024-03-09 12:46:11 -0800

    Tested with DNS Network

--- src/main.c
+++ src/main.c
@@ -4,9 +4,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // number of seconds between every network check
-#define NETWORK_CHECK_INTERVAL  10
+#define NETWORK_CHECK_INTERVAL  5
+
+// arbitrary maximum length of a character array
+#define ARBMAX  1024
 
 int main(int argc, char **argv) {
     printf("Starting the SFCU...\n");
@@ -26,25 +30,51 @@ int main(int argc, char **argv) {
     // run main loop
 
     while (true) {
-        printf("----\n"); // divider
         // 104 is the network ID for SFCU
         char *network_check = dns_network_check("104");
         int n;
         sscanf(network_check, "Check: 104: %d\n", &n);
         free(network_check); // be memory-safe!
-        printf("Got %d unchecked requests.\n", n);
 
-        for (int i = 0; i < n; i++) {
-            printf("\tReceiving request %d of %d...\n", i, n);
-            char *network_request = dns_network_request("104");
-            // handle the unchecked request:
-            // - parse who the vendor is; check against database for dns bank id
-            // - parse who the purchaser is; check against database for dns bank id
-            // - incur debt internally to purchaser; send transfer to vendor from SFCU dns bank account
-            free(network_request);
+        if (n == 0) {
+            // don't clutter stdout if there's no requests; just wait
+            sleep(NETWORK_CHECK_INTERVAL);
+            continue;
         }
 
-        printf("Done checking requests; waiting %d seconds before trying again.\n", NETWORK_CHECK_INTERVAL);
+        printf("----\nGot %d unchecked requests.\n", n);
+
+        for (int i = 0; i < n; i++) {
+            char *network_request = dns_network_request("104");
+            char card_data[ARBMAX], price_str[ARBMAX], vendor[ARBMAX], time[ARBMAX];
+            sscanf(
+                network_request,
+                "Card Data: %s\nPrice: %s\nVendor: %s\nTime: %s\n",
+                card_data,
+                price_str,
+                vendor,
+                time
+            );
+            printf("%s\n", network_request);
+            free(network_request); // be memory-safe!
+
+            // these end with '}'; trim short by one character
+            card_data[strlen(card_data) - 1] = '\0';
+            price_str[strlen(price_str) - 1] = '\0';
+            vendor[strlen(vendor) - 1] = '\0';
+            // NOTE: use str + 1, as each starts with '{'
+
+            printf(
+                "Request %d of %d:\n\tCard Data: %s\n\tPrice: %s\n\tVendor: %s\n",
+                i + 1,
+                n,
+                card_data + 1,
+                price_str + 1,
+                vendor + 1
+            );
+        }
+
+        printf("Done.\n");
         sleep(NETWORK_CHECK_INTERVAL);
     }
 
